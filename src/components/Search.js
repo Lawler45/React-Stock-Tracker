14e38f59b6c29a541fb0ae3b8a050ccf19c81558
Lewis Lawler <lewislawler98@gmail.com>
2023-11-21 14:32:05 +0000

    adds a company search input

---
+++ src/components/Search.js
@@ -0,0 +1,35 @@
+import React, { useState } from "react";
+import { mockSearchResults } from "../constants/mock";
+
+const Search = () => {
+    const [input, setInput] = useState("");
+    const [bestMatches, setBestMatches] = useState(mockSearchResults.results);
+
+    const clear = () => {
+        setInput("");
+        setBestMatches([]);
+    };
+
+    const updateBestMatches = () => {
+        setBestMatches(mockSearchResults.results);
+    };
+
+    return <div className="flex item-center my-4 border-2 rounded-md relative w-96 bg-white border-neutral-200">
+        <input 
+        type="text" 
+        value={input} 
+        className="w-full px-4 py-2 focus:outline-none rounded-md"
+        placeholder="Search stock..."
+        onChange={(event) => {
+            setInput(event.target.value);
+        }}
+        onKeyDown={(event) => {
+            if (event.key === "Enter") {
+                updateBestMatches();
+            }
+        }}
+        />
+    </div>
+};
+
+export default Search; 
