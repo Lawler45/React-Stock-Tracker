0e1f477d852cdec86404d238706994d42c9cd43c
Lewis Lawler <lewislawler98@gmail.com>
2023-11-21 16:49:57 +0000

    adds mock details section

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -2,6 +2,7 @@ import React from "react";
 import Card from "./Card";
 import { mockCompanyDetails } from "../constants/mock";
 import Header from "./Header";
+import Details from "./Details";
 
 const Dashboard = () => {
     return <div className="h-screen grid grid-cols-1 md:grid-cols-2 xl:grid-cols-3 grid-rows-8 md:grid-rows-7 xl:grid-rows-5 auto-rows-fr gap-6 p-10 font-quicksand">
@@ -15,7 +16,7 @@ const Dashboard = () => {
             <Card>Overview</Card>
         </div>
         <div className="row-span-2 xl:row-span-3">
-            <Card>Details</Card>
+            <Details details={mockCompanyDetails} />
         </div>
     </div>
 };
