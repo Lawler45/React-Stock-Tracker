d76132d6fff3ab70eac9b58f2be43c85d95ad308
Lewis Lawler <lewislawler98@gmail.com>
2023-11-27 12:22:34 +0000

    adds fetchQuote endpoint

--- src/api/stock-api.js
+++ src/api/stock-api.js
@@ -23,3 +23,15 @@ export const fetchStockDetails = async (stockSymbol) => {
 
   return await response.json();
 };
+
+export const fetchQuote = async (stockSymbol) => {
+  const url = `${basePath}/quote?symbol=${stockSymbol}&token=${process.env.REACT_APP_API_KEY}`;
+  const response = await fetch(url);
+
+  if (!response.ok) {
+    const message = `An error has occured: ${response.status}`;
+    throw new Error(message);
+  }
+
+  return await response.json();
+};
